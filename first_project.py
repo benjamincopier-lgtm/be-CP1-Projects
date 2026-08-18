cc91766e0b77f589c448a38ecdc58f1430f5eb0a
benjamincopier-Igtm <benjamin.copir@ucas-edu.net>
2026-08-18 09:12:47 -0600

    first commit

---
+++ first_project.py
@@ -0,0 +1,11 @@
+# Benjamin copier, first project
+
+
+x=int(input("write a number: "))
+y=x
+str= 5*x
+print(str)
+#name=input("what is your name: ")
+#print ("welcome to class " + name)
+if( str<250 ):print("steve")
+if( str>=250):print("carl") 
