9d5585e1633d0416d1bc0f970656c210da77a1ec
benjamincopier-Igtm <benjamin.copir@ucas-edu.net>
2026-08-19 09:04:23 -0600

    hello world assignment

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+# Benjamin copier, hello world
+
+
+print("you wake up in a room tha you do not recognise, when you look around you see that there is a door to the left of you and a random person to the right of you")
+name= input("they ask you 'hello whats your name': ")
+print("hello " + name)
+choices=input("do you wish to leave this room (hint you can use the word left to go left and vice verse with right): " )
+if (choices == "left"):print("you walk out and you see that you are in a new city")
+c1=input
