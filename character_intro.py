2be7ec32d66b80fd32c29a24c3733d85addace0d
benjamincopier-Igtm <benjamin.copir@ucas-edu.net>
2026-08-20 09:38:08 -0600

    i made the character intro

---
+++ character_intro.py
@@ -0,0 +1,7 @@
+# Benjamin Copier character introduction
+
+name=input("tell me your name: ")
+age=int(input("what is your age: "))
+job=input("what job do you want: ")
+home=input("where are you from:")
+print("this is " + name + " he is from " + home + " he is", age , "years old, and the job he wants is " + job )
