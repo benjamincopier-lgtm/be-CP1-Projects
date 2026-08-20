0cefe4bee41732b30911a89e6456f74f3b7cf478
benjamincopier-Igtm <benjamin.copir@ucas-edu.net>
2026-08-20 09:05:55 -0600

    deleted first_project.py and added more choices

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,13 @@
 # Benjamin copier, hello world
 
-
+#cx=choices (x=any number)
 print("you wake up in a room tha you do not recognise, when you look around you see that there is a door to the left of you and a random person to the right of you")
 name= input("they ask you 'hello whats your name': ")
-print("hello " + name)
+print( "hello " + name)
 choices=input("do you wish to leave this room (hint you can use the word left to go left and vice verse with right): " )
 if (choices == "left"):print("you walk out and you see that you are in a new city")
-c1=input
+print("there are three paths one in front of you, one to the left of you, and one to the right of you")
+choices=input("if you wish to go to the left type left, if you want to go to the right type right and if you want to go forward type front: " )
+if (choices == "left"):print("you go to the left")
+if (choices == "right"):print("you go to the right")
+if (choices ==)
---
+++ variables.py
@@ -0,0 +1,26 @@
+# benjamin copier, variables notes
+
+# create a variable
+name= ("eagle")
+
+print("hello " + name)
+
+
+name = ("little chad")
+
+print("sorry I meant " + name)
+
+
+
+age = 14
+
+print("wow you are", age)
+print("oh wait your birthday was yesterday?")
+age += 1
+print("so you are", age)
+#snake case all lower case, and all spaces= _
+#camelcase= capitolise the first letter of all words,andnospacesfor naming functions
+#pascalcase= capitolise the first letter of all words no space for classes
+
+#names= start with a letter _ no special charectors can have numbers short and specific less then two word variables cannot be an identifier
+#scope= put variables at the top then we can use everywhere global variable can be used anywhere local varis kill global locals can only be used in single lines
