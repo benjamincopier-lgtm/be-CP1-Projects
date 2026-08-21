131a1006edd6ac65e77219aa95193e15021d119f
benjamincopier-Igtm <benjamin.copir@ucas-edu.net>
2026-08-21 09:20:49 -0600

    fixed how name was spelled and added the missing parenthesis

--- debugging.py
+++ debugging.py
@@ -1,4 +1,8 @@
+#benjamin copier debuging
+
+
 name = "Alex"
 age = 16
 
 print("My name is " + name + " and I am " + str(age) + " years old.")
+print ("i accidentely pushed to early and require change in the code")
