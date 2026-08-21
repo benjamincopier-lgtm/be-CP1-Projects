7ed60cc6fddb3da6efe6daafed3e9e283e787f99
benjamincopier-Igtm <benjamin.copir@ucas-edu.net>
2026-08-21 09:51:59 -0600

    learned notes

---
+++ notes_new.py
@@ -0,0 +1,15 @@
+#debuging is the proces of removing errors from your code.
+#syntax errors=wrote it wrong
+#ex
+
+#indentation error indenton same line
+#wite space is empty thing in your code
+#logic error is hard bacause it doesn work even though you did all of the steps read all steps
+#run time error
+while True:
+    try:
+     fav_num = int(input("what is your favorite number: "))
+    except:
+     print("you stupid")
+    else:
+        break
