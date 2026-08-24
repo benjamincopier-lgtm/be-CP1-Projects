4adcf9b03025785f803f3ea27c39ab8b43b5d0c1
benjamincopier-Igtm <benjamin.copir@ucas-edu.net>
2026-08-24 09:18:13 -0600

    I saw that the end of the print statement was missing a parenthesis. The variable "name" was misspelled inside of the print statement, it was missing an "E" so I added the "E".

--- debugging.py
+++ debugging.py
@@ -4,5 +4,5 @@
 name = "Alex"
 age = 16
 
-print("My name is " + name + " and I am " + str(age) + " years old.")
-
+print("My name is " + name + " and I am " + str(age) + " years old.")#I saw that the end of the print statement was missing a parenthesis.
+#The variable "name" was misspelled inside of the print statement, it was missing an "E" so I added the "E".
