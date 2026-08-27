2410e220b951016f8589da950979f1826ce3b57f
benjamincopier-Igtm <benjamin.copir@ucas-edu.net>
2026-08-27 09:34:02 -0600

    I completed the unit 1 final project

---
+++ unit1_final_project.py
@@ -0,0 +1,6 @@
+#Benjamin copier unit 1 final project.
+name=input("what is your name?: ")
+home=input("where do you come from?: ")
+invitation=input("who Invited you?: ")
+favorite_food=input("what kind of food would you want?: ")
+print("I have a speech to give at the moment if you will excuse me, and my butler will be getting you that " + favorite_food + " hello everyone, today we have a very special guest whose name is " + name + " he is from the great place called " + home + " he was invited by the honored "+ invitation + " and we are all very honored that he would come to see us.")
