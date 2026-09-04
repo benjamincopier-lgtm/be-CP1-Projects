22a7ca468a49cafd3573db788bb9411c28d31981
benjamincopier-lgtm <benjamin.copier@ucas-edu.net>
2026-09-04 09:53:23 -0600

    notes

---
+++ notes_3.py
@@ -0,0 +1,14 @@
+#str methods
+#make a string first
+#methhead write what you want to be thingied dot action called dot notation, lower capitalize, upper, and title
+#user=stupid
+#methheads change not the variable
+#
+#
+name=input("what is your name: ").strip().title()
+print("hello " + name.strip().title())
+sentence = "the Quick brown fox jumps over the lazy dog"
+#print(sentence.capitalize())
+fixed=sentence.replace("fox", "wolf")
+print(fixed)
+print(sentence.split("the"))
