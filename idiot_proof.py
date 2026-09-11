b37becabe8af6979f7af33491ac55a29a55dc124
benjamincopier-lgtm <benjamin.copier@ucas-edu.net>
2026-09-11 09:41:15 -0600

    i added the grade

--- idiot_proof.py
+++ idiot_proof.py
@@ -18,3 +18,64 @@ print(new_phone2)
 new_phone3= str(sphone[6:10])
 print(new_phone3)
 print("your phone number is " + new_phone+"-"+new_phone2+"-"+new_phone3)
+while True:
+    try:
+        grade_1 =float(input("first period grade: "))
+    except:
+     print("not a number")
+    else:
+        break
+print(grade_1)
+while True:
+    try:
+        grade_2 =float(input("second period grade: "))
+    except:
+     print("not a number")
+    else:
+        break
+print(grade_2)
+while True:
+    try:
+        grade_3 =float(input("third period grade: "))
+    except:
+     print("not a number")
+    else:
+        break
+print(grade_3)
+while True:
+    try:
+        grade_4 =float(input("fourth period grade: "))
+    except:
+     print("not a number")
+    else:
+        break
+print(grade_4)
+while True:
+    try:
+        grade_5 =float(input("fith period grade: "))
+    except:
+     print("not a number")
+    else:
+        break
+print(grade_5 )
+while True:
+    try:
+        grade_6 =float(input("sixth period grade: "))
+    except:
+     print("not a number")
+    else:
+        break
+print(grade_6)
+while True:
+    try:
+        grade_7 =float(input("seventh period grade: "))
+    except:
+     print("not a number")
+    else:
+        break
+print(grade_7)
+total_numb= grade_1 + grade_2 + grade_3 + grade_4 + grade_5 + grade_6 + grade_7
+average_grade= float( total_numb/7)
+print("unrounded grade" , average_grade)
+rounded_grades= round(average_grade,1)
+print("the rounded grade is" ,rounded_grades)
