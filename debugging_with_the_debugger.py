1055d8393cc37d58d6520aa1377dc2ba71b6f192
benjamincopier-lgtm <benjamin.copier@ucas-edu.net>
2026-09-17 09:53:52 -0600

    on line 22 the parenthesis was not closed, on line 19 the variable was missnamed because the underscore was missing and name was capitolized. on line eleven total is not an integer, wich effects line thirteen, because a string connot be subtracted from by an integer, so i made it into an integer. the total before tax was not the total it was just the price. there was also a string that was supposed to be an integer

--- debugging_with_the_debugger.py
+++ debugging_with_the_debugger.py
@@ -6,7 +6,7 @@ pirate_name = input("What's your name, pirate? ")
 snack_name = input("What snack do you want? ")
 
 price = random.randint(2, 8)  # random price in credits
-quantity = input("How many would you like? ")
+quantity = int(input("How many would you like? "))# it was acting as if it was a string.
 
 total = price * quantity
 total2=int(total)#made into an integer
@@ -18,5 +18,5 @@ total_with_tax = discounted_total + (discounted_total * tax_rate)
 print("Hello, " + pirate_name + "! Here's your order summary:")
 print("Snack: " + snack_name)#renamed the variable
 print("Price per snack: " + str(price) + " credits")
-print("Total before tax: " + int(discounted_total)* int(quantity))#the total before tax was not the total it was just the price. it also was not the true total due to not including the discount.
+print("Total before tax: " + str(discounted_total))#the total before tax was not the total it was just the price.
 print("Total with tax: " + str(round(total_with_tax, 2)) + " credits")#added a parenthesis.
