e05b509c24f35b749d46c76045466485485a225a
benjamincopier-lgtm <benjamin.copier@ecas-edu.net>
2026-09-23 09:55:37 -0600

    cheesus is evil

--- crew_share.py
+++ crew_share.py
@@ -13,3 +13,22 @@ ravagers_without_leaders= ravagers_with_leaders- (2)
 print(ravagers_without_leaders)
 ravagers_first_cut= ravagers_without_leaders * (3)
 new_credits= credits_earned -ravagers_first_cut
+one_percent_of_new_credits= new_credits /100 
+yondus_cut= 13* one_percent_of_new_credits
+rounded_yondus_cut= round(yondus_cut,2)
+new_credits2= new_credits-rounded_yondus_cut
+one_percent_of_new_credits2=new_credits2/100
+peters_cut= one_percent_of_new_credits2 *11
+rounded_peters_cut= round(peters_cut,2)
+new_credits3= new_credits2-rounded_peters_cut
+ravagers_with_leaders_cut= new_credits3/ ravagers_with_leaders
+rounded_ravagers_with_leaders_cut=round(ravagers_with_leaders_cut, 2)
+yondus_new_cut= rounded_yondus_cut + rounded_ravagers_with_leaders_cut
+peters_new_cut=rounded_peters_cut + rounded_ravagers_with_leaders_cut
+string_ravagers_with_leaders_cut=str(ravagers_with_leaders_cut)
+string_peters_new_cut= str(peters_new_cut)
+string_yondus_new_cut = str(yondus_new_cut)
+
+print( f"all of the ravagers have at least gotten " +string_ravagers_with_leaders_cut)
+print(f"peter in total got "+string_peters_new_cut)
+print(F"yondu got in total " + string_yondus_new_cut)
