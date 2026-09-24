b565d12967181e26612126f724c23c276def837a
benjamincopier-lgtm <benjamin.copier@ecas-edu.net>
2026-09-24 09:05:43 -0600

    I completed the ravager cut assignment

--- crew_share.py
+++ crew_share.py
@@ -25,10 +25,12 @@ ravagers_with_leaders_cut= new_credits3/ ravagers_with_leaders
 rounded_ravagers_with_leaders_cut=round(ravagers_with_leaders_cut, 2)
 yondus_new_cut= rounded_yondus_cut + rounded_ravagers_with_leaders_cut
 peters_new_cut=rounded_peters_cut + rounded_ravagers_with_leaders_cut
-string_ravagers_with_leaders_cut=str(ravagers_with_leaders_cut)
-string_peters_new_cut= str(peters_new_cut)
-string_yondus_new_cut = str(yondus_new_cut)
+rounded_peters_new_cut= round(peters_new_cut,2)
+yondus_rounded_new_cut=round(yondus_new_cut,2)
+string_rounded_ravagers_with_leaders_cut=str(rounded_ravagers_with_leaders_cut)
+string_peters_new_cut= str(rounded_peters_new_cut)
+string_yondus_new_cut = str(yondus_rounded_new_cut)
 
-print( f"all of the ravagers have at least gotten " +string_ravagers_with_leaders_cut)
+print( f"all of the ravagers have at least gotten " +string_rounded_ravagers_with_leaders_cut)
 print(f"peter in total got "+string_peters_new_cut)
 print(F"yondu got in total " + string_yondus_new_cut)
